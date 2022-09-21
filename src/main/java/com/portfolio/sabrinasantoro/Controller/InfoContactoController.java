211f356204f8405db94b1a736e035ee39c124d99
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-09-21 01:44:22 -0300

    Update InfoContactoController.java

--- src/main/java/com/portfolio/sabrinasantoro/Controller/InfoContactoController.java
+++ src/main/java/com/portfolio/sabrinasantoro/Controller/InfoContactoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/infodecontacto")
-@CrossOrigin (origins = {"https://portfoliosantoro.web.app", "http://localhost:4200"})
+@CrossOrigin (origins = "*")
 public class InfoContactoController {
     
   @Autowired 
