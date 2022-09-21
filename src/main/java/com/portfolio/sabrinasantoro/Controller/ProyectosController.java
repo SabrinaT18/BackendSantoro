bd5a9b4891a21269ded8bfac2022f528ced6dd0a
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-09-21 01:44:47 -0300

    Update ProyectosController.java

--- src/main/java/com/portfolio/sabrinasantoro/Controller/ProyectosController.java
+++ src/main/java/com/portfolio/sabrinasantoro/Controller/ProyectosController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/proyectos")
-@CrossOrigin(origins = {"https://portfoliosantoro.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = "*")
 public class ProyectosController {
 
     @Autowired
