d355fd38ea1740f75cefa85a89fc535b112d3d68
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-11-11 16:40:05 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/sabrinasantoro/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/sabrinasantoro/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins ="https://portfoliosantoro.web.app")
+@CrossOrigin
 public class AuthController {
 
     @Autowired
